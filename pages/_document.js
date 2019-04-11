698f55393af907969fbfa90d4a37dc03782a6912
Fernando Obieta <fernando.obieta@blanktree.ch>
2019-04-11 11:52:42 +0200

    pressure... I am not sure how the next couple of hours are going to be...

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,6 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
-  constructor(props) {
-    super(props);
-  }
-
   render () {
     return (
       <html>
